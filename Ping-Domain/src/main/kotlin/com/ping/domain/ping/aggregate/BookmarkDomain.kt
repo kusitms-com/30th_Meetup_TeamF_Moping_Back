f807152ecd3318fa5cdd0895b8b5bd4d75064d4f
codrin2 <hssang127@gmail.com>
2024-11-27 03:17:09 +0900

    feat: 멤버 핑 추가 로직 중 domain 특화 로직 분리

--- Ping-Domain/src/main/kotlin/com/ping/domain/ping/aggregate/BookmarkDomain.kt
+++ Ping-Domain/src/main/kotlin/com/ping/domain/ping/aggregate/BookmarkDomain.kt
@@ -1,5 +1,9 @@
 package com.ping.domain.ping.aggregate
 
+import com.ping.common.exception.CustomException
+import com.ping.common.exception.ExceptionContent
+import com.ping.domain.ping.repository.BookmarkRepository
+
 data class BookmarkDomain (
     val name: String,
     val px: Double,
@@ -8,4 +12,12 @@ data class BookmarkDomain (
     val address: String,
     val mcidName: String,
     val url: String,
-)
+) {
+    companion object {
+        fun validateSidExists(sid: String, bookmarkRepository: BookmarkRepository) {
+            if (!bookmarkRepository.existsBySid(sid)) {
+                throw CustomException(ExceptionContent.NON_MEMBER_PLACE_NOT_FOUND)
+            }
+        }
+    }
+}
