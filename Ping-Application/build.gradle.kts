58bdabf2f83759be13717e2d17d4ae7454fdfeea
codrin2 <hssang127@gmail.com>
2024-11-26 13:46:54 +0900

    feat: Support module 추가에 따른 관련 module 업데이트

--- Ping-Application/build.gradle.kts
+++ Ping-Application/build.gradle.kts
@@ -2,9 +2,11 @@ dependencies {
     implementation(project(":Ping-Common"))
     implementation(project(":Ping-Domain"))
     implementation(project(":Ping-Client"))
+    implementation(project(":Ping-Support"))
 
     //jpa
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation(project(mapOf("path" to ":")))
 }
 tasks {
     bootJar {
--- Ping-Common/build.gradle.kts
+++ Ping-Common/build.gradle.kts
@@ -1,9 +1,4 @@
 dependencies {
-    // JWT
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-    implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
-    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-
     //jpa
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
