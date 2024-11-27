0f1fc8b3996f2c43fce03cfaf6ddaf6407d23ee2
codrin2 <hssang127@gmail.com>
2024-11-27 17:01:42 +0900

    fix: 을지로 매칭 값으로 을지로4가 추가

--- Ping-Common/src/main/kotlin/com/ping/common/util/AlternativeQueryProvider.kt
+++ Ping-Common/src/main/kotlin/com/ping/common/util/AlternativeQueryProvider.kt
@@ -2,7 +2,7 @@ package com.ping.common.util
 
 object AlternativeQueryProvider {
     private val alternativeQueries = mapOf(
-        "을지로" to listOf("을지로3가", "을지로입구")
+        "을지로" to listOf("을지로3가", "을지로4가", "을지로입구")
     )
 
     fun getQueries(keyword: String): List<String> {
